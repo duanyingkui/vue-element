22c20c169a5c27a40b99a1bac6f85bd1d82eda89
duanyingkui <duanyingkui0905@163.com>
2019-09-05 10:13:10 +0800

    feat: css

--- src/apps/login/views/login.vue
+++ src/apps/login/views/login.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-carousel arrow="never">
+    <el-carousel :height="bannerHeight + 'px'" arrow="never">
         <el-carousel-item v-for="item in 4" :key="item">
             <h3>{{ item }}</h3>
         </el-carousel-item>
@@ -12,8 +12,16 @@ import Component from 'vue-class-component';
 export default @Component()
 
 class Login extends Vue {
+    bannerHeight = '';
 
+    mounted() {
+        this.bannerHeight = document.documentElement.clientHeight;
+        window.onresize = () => {
+            this.bannerHeight = document.documentElement.clientHeight;
+        };
+    }
 }
+
 </script>
 <style lang="scss" scoped>
 .el-carousel__item h3 {
@@ -31,4 +39,4 @@ class Login extends Vue {
 .el-carousel__item:nth-child(2n+1) {
     background-color: #d3dce6;
 }
-</style>>
+</style>
